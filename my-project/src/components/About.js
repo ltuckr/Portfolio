d852ece3616c37d4a3254af203d3b48ba9bf542d
Lauren Tucker <l.tucker.rdh@gmail.com>
2023-09-04 18:47:53 -0400

    added projects and contact info

--- my-project/src/components/About.js
+++ my-project/src/components/About.js
@@ -3,7 +3,7 @@ import AboutImg from "../assets/about-img.png";
 
 const About = () => {
   return (
-    <section className="bg-secondery text-white px-5 py-32" id="about">
+    <section className="bg-secondary text-white px-5 py-32" id="about">
       <div className="container mx-auto grid md:grid-cols-2 items-center justify-center md:justify-between">
         <div className="about-info">
           <h2 className="text-4xl font-bold mb-5 border-b-[5px] w-[180px] border-indigo-600 pb-2">
@@ -13,10 +13,9 @@ const About = () => {
           <p className="pb-5">
         
             Hey there! I'm Lauren. I'm a Fullstack Developer with a passion for Frontend. 
-            I'm currently looking for full-time employment in web dev, and the opportunity to flex my creative muscles. 
-             Making things look good is my happy place.
-            I have a certificate from the University of North Carolina at Charlotte in Fullstack Development, 
-            and a BFA in commercial photography from The Art Institute of Charlotte. 
+            I'm currently looking for full-time employment in web dev, and want the opportunity to flex my creative muscles. 
+            Making things look good is my happy place. I have a certificate from the University of North Carolina at Charlotte in Fullstack Development, 
+            and a Bachelor of Fine Arts in commercial photography from The Art Institute of Charlotte. 
 
           </p>
           <p className="pb-5">
---
+++ my-project/src/components/Project.js
@@ -0,0 +1,86 @@
+//project array
+const projects = [
+    {
+      img: frostBite,
+      title: "Frostbite",
+      desc: "",
+      live: "",
+      code: "",
+    },
+    {
+      img: flavorImpersonator,
+      title: "Flavor Impersonator",
+      desc: "",
+      live: "",
+      code: "",
+    },
+    {
+      img: Project 3,
+      title: "Project 3",
+      desc: "",
+      live: "",
+      code: "",
+    },
+
+  ]; 
+
+//continue component here
+import React from "react";
+import cssProjects from "../assets/cssprojects.png";
+import devlog from "../assets/devlog.png";
+import getInspirred from "../assets/get-inspirred.png";
+import uilogs from "../assets/uilogs.png";
+
+const Projects = () => {
+  const projects = [...];
+
+  return (
+    <section className="bg-primary text-white px-5 py-32" id="projects">
+      <div className="container mx-auto grid md:grid-cols-2 items-center md:justify-between">
+        <div className="about-info mb-5">
+          <h2 className="text-4xl font-bold mb-5 border-b-[5px] w-[180px] border-indigo-600 pb-2">
+            Projects
+          </h2>
+
+          <p className="pb-5">
+            Here are some of my projects. I'm always working on something new, so check back often!
+          </p>
+        </div>
+
+        <div className="about-img"></div>
+      </div>
+
+      <div className="projects container mx-auto grid md:grid-cols-3 gap-10">
+        {projects.map((project, i) => {
+          return (
+            <div className="relative" key={i}>
+              <img src={project.img} alt={project.title} />
+              <div className="flex absolute left-0 right-0 top-[13px] bottom-0 mx-auto w-[90%] h-[90%]  bg-primary  opacity-0 duration-500 justify-center flex-col hover:opacity-100 ">
+                <p className="py-5 text-center font-bold px-2 text-white">
+                  {project.desc}
+                </p>
+
+                <div className="mx-auto">
+                  <a
+                    href={project.live}
+                    className="px-5 py-2 bg-blue-500 hover:bg-blue-600 mr-5 font-bold"
+                  >
+                    Live
+                  </a>
+                  <a
+                    href={project.code}
+                    className="px-5 py-2 bg-blue-700 hover:bg-blue-800 font-bold"
+                  >
+                    Code
+                  </a>
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </section>
+  );
+};
+
+export default Projects;
