f29b12b412f7d2a98aa20b82afd2c0be9833c829
Lauren Tucker <l.tucker.rdh@gmail.com>
2023-09-04 11:30:26 -0400

    created and imported header.js

--- my-project/src/App.js
+++ my-project/src/App.js
@@ -1,11 +1,12 @@
 
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
-    <div className="App">
-      Hello world!
-    </div>
+    <>
+      <Header/>
+    </>
   );
 }
 
