5ad31f5a6bbad42e80119be6333514015e23b511
Lauren Tucker <l.tucker.rdh@gmail.com>
2023-09-04 18:17:47 -0400

    added header styling

--- my-project/src/App.js
+++ my-project/src/App.js
@@ -1,11 +1,23 @@
 
-import './App.css';
-import Header from './components/Header';
+import "./App.css";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import About from "./components/About";
+import Projects from "./components/Projects";
+import Blog from "./components/Blog";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
-      <Header/>
+      <Header />
+      <Hero />
+      <About />
+      <Projects />
+      <Blog />
+      <Contact />
+      <Footer />
     </>
   );
 }
