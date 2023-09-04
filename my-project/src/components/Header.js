511b8f954ec49a4775c57f05b8a705f726518697
Lauren Tucker <l.tucker.rdh@gmail.com>
2023-09-04 18:53:54 -0400

    cleaned up header code boilerplate, added personalization

--- my-project/src/components/Header.js
+++ my-project/src/components/Header.js
@@ -1,6 +1,6 @@
-
 import React, { useState } from "react";
 import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   const [toggle, setToggle] = useState(false);
@@ -10,7 +10,7 @@ const Header = () => {
   return (
     <header className="flex justify-between px-5 py-2 bg-primary text-white fixed w-full z-10">
       <a href="/" className="logo text-2xl font-bold text-accent">
-        Amrin
+        Lauren Tucker
       </a>
 
       {/* Desktop Nav */}
@@ -26,7 +26,12 @@ const Header = () => {
             <a href="/#contact">Contact</a>
           </li>
           <li>
-            <a href="#resume -link" target="_blank" without rel="noreferrer">
+            <a
+              href="https://doc"
+              target="_blank"
+              without
+              rel="noreferrer"
+            >
               Resume
             </a>
           </li>
