da8d9971da373c35721ffcebfdebef6ea64948e5
Lauren Tucker <l.tucker.rdh@gmail.com>
2023-09-07 16:06:00 -0400

    image size and padding change in about

--- my-project/src/components/About.js
+++ my-project/src/components/About.js
@@ -3,7 +3,7 @@ import AboutImg from "../assets/About.jpg";
 
 const About = () => {
   return (
-    <section className="bg-secondary text-purple px-5 py-32" id="about">
+    <section className="bg-secondary text-white px-5 py-32" id="about">
       <div className="container mx-auto grid md:grid-cols-2 items-center justify-center md:justify-between">
         <div className="about-info">
           <h2 className="text-4xl font-bold mb-5 border-b-[5px] w-[180px] border-indigo-600 pb-2">
@@ -33,7 +33,7 @@ const About = () => {
             src={AboutImg} 
             
             alt="female coder and her cat illustration"
-            className="lgw-[80%] md:ml-auto"
+            className="lg:w-[70%] md:ml-auto pl-4"
           />
         </div>
       </div>
