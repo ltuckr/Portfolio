58774d3a95d8b1e4ce29d85f7c46713b0c674d8c
Lauren Tucker <l.tucker.rdh@gmail.com>
2023-09-07 12:52:37 -0400

    corrected image caps error

--- my-project/src/App.js
+++ my-project/src/App.js
@@ -9,22 +9,22 @@ function App() {
   // Create an array of project data
   const projects = [
     {
-      title: "Project 1",
-      img: "project1.jpg",
+      title: "Frostbite",
+      img: "Frostbite.png",
       desc: "Description of Project 1",
       live: "https://example.com/project1",
       code: "https://github.com/user/project1",
     },
     {
-      title: "Project 2",
-      img: "project2.jpg",
+      title: "Flavor Impersonator",
+      img: "flavorImpersonator.png",
       desc: "Description of Project 2",
       live: "https://example.com/project2",
       code: "https://github.com/user/project2",
     },
     {
       title: "Project 3",
-      img: "project3.jpg",
+      img: "TBD.jpg",
       desc: "Description of Project 3",
       live: "https://example.com/project3",
       code: "https://github.com/user/project3",
