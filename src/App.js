57091edd9eee1ebbc4109aa51b9e1145cd6581e8
Lauren Tucker <l.tucker.rdh@gmail.com>
2023-09-14 11:25:19 -0400

    removed hashrouter, trying deploy with renamed build to doc folder

--- src/App.js
+++ src/App.js
@@ -3,22 +3,21 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Home from "./page/Home";
 import Projects from "./components/Projects"; 
-import { HashRouter, Routes, Route } from "react-router-dom";
+
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <HashRouter>
-      <>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/projects" element={<Projects />} />
-        </Routes>
-        <Footer />
-      </>
-    </HashRouter>
+    <>
+      <Header />
+      <Routes>
+  <Route path="/" element={<Home />} />
+  <Route path="/projects" element={<Projects />} />
+</Routes>
+
+      <Footer />
+    </>
   );
 }
 
 export default App;
-
