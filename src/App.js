cedd46d327a466e3c6a0e2d4cf95b2c1886dbea8
Lauren Tucker <l.tucker.rdh@gmail.com>
2023-09-14 10:52:37 -0400

    wrapped routes in hashrouter

--- src/App.js
+++ src/App.js
@@ -3,21 +3,22 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Home from "./page/Home";
 import Projects from "./components/Projects"; 
-
-import { Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <>
-      <Header />
-      <Routes>
-  <Route path="/" element={<Home />} />
-  <Route path="/projects" element={<Projects />} />
-</Routes>
-
-      <Footer />
-    </>
+    <HashRouter>
+      <>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/projects" element={<Projects />} />
+        </Routes>
+        <Footer />
+      </>
+    </HashRouter>
   );
 }
 
 export default App;
+
